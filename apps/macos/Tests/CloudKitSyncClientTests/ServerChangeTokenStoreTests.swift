d3a782492f299c36a47be56a79f30056fea7d09f
ZingerLittleBee <6970999@gmail.com>
2026-05-02 13:21:57 +0800

    cloudkit: implement StoredServerChangeToken garbage-input test

--- apps/macos/Tests/CloudKitSyncClientTests/ServerChangeTokenStoreTests.swift
+++ apps/macos/Tests/CloudKitSyncClientTests/ServerChangeTokenStoreTests.swift
@@ -11,7 +11,8 @@ final class StoredServerChangeTokenTests: XCTestCase {
 		throw XCTSkip("requires FakeCloudDatabase.makeRealishToken from Task 1.5")
 	}
 
-	func testUnarchiveOnGarbageThrows() throws {
-		throw XCTSkip("requires FakeCloudDatabase.makeRealishToken from Task 1.5")
+	func testUnarchiveOnGarbageThrows() {
+		let stored = StoredServerChangeToken(archivedData: Data([0xDE, 0xAD, 0xBE, 0xEF]))
+		XCTAssertThrowsError(try stored.unarchive())
 	}
 }
